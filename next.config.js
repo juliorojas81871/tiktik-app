98e48ba4e3c1d46368831b7799b9670af7db664b
Julio Rojas <juliorojas81871@gmail.com>
2022-11-19 20:58:43 -0500

    finish with noResults and videoscard

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript:{
-    ignoreBuildErrors:true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["media-exp1.licdn.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
