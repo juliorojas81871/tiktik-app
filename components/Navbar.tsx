8d2649f933225c5bf2dae2fd9861074742ad4fc6
Julio Rojas <juliorojas81871@gmail.com>
2022-12-05 12:48:52 -0500

    search bar change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,8 +30,8 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
-      <div className="hidden md:inline">
+    <div>
+      <div className="w-full flex justify-between items-center md:border-b-2 border-gray-200 py-2 px-4">
         <Link href="/">
           <div className="w-[100px] md:w-[129px]">
             <Image
@@ -44,58 +44,65 @@ const Navbar = () => {
             />
           </div>
         </Link>
-      </div>
-      <div className="inline md:hidden">
-        <Link href="/">
-          <div className="w-[50px]">
-            <Image
-              className="cursor-pointer"
-              src="/tiktik-icon.png"
-              alt="logo"
-              width={40}
-              height={40}
-              layout="responsive"
-            />
-          </div>
-        </Link>
-      </div>
 
-      <div className="relative md:block ">
-        <form
-          onSubmit={handleSearch}
-          className="md:static md:top-10 -left-20 bg-white dark:bg-[#0f1217]"
-        >
-          <input
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            className="bg-primary dark:bg-gray-800 p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[200px] md:w-[350px] rounded-full md:top-0"
-            // placeholder="Search accounts and videos"
-          />
-          <button
-            onClick={handleSearch}
-            className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
+        <div className="hidden md:inline md:relative ">
+          <form
+            onSubmit={handleSearch}
+            className="static top-10 -left-20 bg-white dark:bg-[#0f1217]"
           >
-            <BiSearch />
-          </button>
-        </form>
+            <input
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              className="bg-primary dark:bg-gray-800 p-3 text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[350px] rounded-full top-0"
+              placeholder="Search accounts and videos"
+            />
+            <button
+              onClick={handleSearch}
+              className="absolute  right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
+            >
+              <BiSearch />
+            </button>
+          </form>
+        </div>
+        <div>
+          {user ? (
+            <div className="flex gap-5 md:gap-10">
+              <Link href="/upload">
+                <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+                  <IoMdAdd className="text-xl" />{" "}
+                  <span className="hidden md:block">Upload </span>
+                </button>
+              </Link>
+              <Dropdown user={user} />
+            </div>
+          ) : (
+            <GoogleLogin
+              onSuccess={(response) => createOrGetUser(response, addUser)}
+              onError={() => console.log("Login Failed")}
+            />
+          )}
+        </div>
       </div>
-      <div>
-        {user ? (
-          <div className="flex gap-5 md:gap-10">
-            <Link href="/upload">
-              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
-                <IoMdAdd className="text-xl" />{" "}
-                <span className="hidden md:block">Upload </span>
-              </button>
-            </Link>
-            <Dropdown user={user} />
-          </div>
-        ) : (
-          <GoogleLogin
-            onSuccess={(response) => createOrGetUser(response, addUser)}
-            onError={() => console.log("Login Failed")}
-          />
-        )}
+      <div className="w-full flex md:hidden flex-col justify-center  items-center border-b-2 border-gray-200 py-2 px-4">
+        <div className="relative">
+          <form
+            onSubmit={handleSearch}
+            className="static -left-20 bg-white dark:bg-[#0f1217]"
+          >
+            <input
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              className="bg-primary dark:bg-gray-800 p-3 font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[350px] rounded-full "
+              placeholder="Search accounts and videos"
+            />
+            <button
+              onClick={handleSearch}
+              className="absolute right-2 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
+            >
+              <BiSearch />
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
